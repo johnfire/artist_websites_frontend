e3e58a01fb11429bdb65a678f53fef2e94a436ae
christopher rehm <christopherrehm@web.de>
2023-03-28 08:40:28 +0200

    added login click handler function

--- src/components/UserLoginButton.tsx
+++ src/components/UserLoginButton.tsx
@@ -2,9 +2,16 @@ import React from "react";
 
 const UserLoginButton = () => {
   console.log("ULB5 here is user login button");
+
+  const handleClick = () => {
+    console.log("ULB7 clicked login button ");
+  };
+
   return (
     <>
-      <button type="button">login?</button>
+      <button type="button" onClick={handleClick}>
+        login?
+      </button>
     </>
   );
 };
