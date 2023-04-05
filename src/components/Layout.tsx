11e0919c99fc017390c81f600c7f6bac235dbb3d
christopher rehm <christopherrehm@web.de>
2023-04-05 18:12:57 +0200

    bug notice react 18

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactComponentElement } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-/* @ts-ignore */
+/* @ts-ignore */ // ReactFC cannot have child compponents in typescript, its a react 18 bug.....
 const Layout: React.FC = ({ children }: React.ReactElement) => {
   console.log("layout 5");
   return (
